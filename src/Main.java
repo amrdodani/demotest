427363a26a2a02a60b1016f261c8730b8b55b61b
Code.in <amrdodani@gmail.com>
2025-02-26 11:48:54 -0600

    2bd cmmit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-public class Main {
-    public static void main(String[] args)
+import java.util.Scanner;
 
-    {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + " " + "\uD83D\uDE02" +"\uD83D\uDE48");
+
+        scanner.close(); // Close the scanner to avoid resource leaks
     }
 }
